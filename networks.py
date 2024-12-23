864738299a70e64ac021d9a8539cb464fd1fdc44
Thibaut <thibaut2saivre@gmail.com>
2024-12-23 15:48:47 +0100

    Finish conditional discriminator implementation

--- networks.py
+++ networks.py
@@ -22,7 +22,6 @@ class Parameters:
     z_size: int = 100  # Size of the input noise vector
     n_classes: int = 10  # Number of classes in the dataset (for conditional GAN)
     feature_map_size: int = 64  # Size of the feature maps in the generator & discriminator
-    channels: int = 1  # Number of channels in the input images
     epochs: int = 5  # Amount of training epochs
     lr: float = 0.0002  # Learning rate
     beta1: float = 0.5  # Beta1 for Adam optimizer
@@ -34,7 +33,7 @@ class Generator(nn.Module):
         self.params = params
 
         self.main = nn.Sequential(
-            # (batch_size, params.z_size, 1, 1) -> (batch_size, params.feature_map_size * 2, 7, 7)
+            # (batch_size, params.z_size + params.n_channels, 1, 1) -> (batch_size, params.feature_map_size * 2, 7, 7)
             nn.ConvTranspose2d(params.z_size + params.n_classes, params.feature_map_size * 2, 7, 1, 0, bias=False),
             nn.BatchNorm2d(params.feature_map_size * 2),
             nn.ReLU(True),
@@ -42,8 +41,8 @@ class Generator(nn.Module):
             nn.ConvTranspose2d(params.feature_map_size * 2, params.feature_map_size, 4, 2, 1, bias=False),
             nn.BatchNorm2d(params.feature_map_size),
             nn.ReLU(True),
-            # (batch_size, params.feature_map_size, 14, 14) -> (batch_size, params.channels, 28, 28)
-            nn.ConvTranspose2d(params.feature_map_size, params.channels, 4, 2, 1, bias=False),
+            # (batch_size, params.feature_map_size, 14, 14) -> (batch_size, channels=1, 28, 28)
+            nn.ConvTranspose2d(params.feature_map_size, 1, 4, 2, 1, bias=False),
             nn.Tanh(),
             # output: (batch_size, params.channels, 28, 28)
         )
@@ -55,9 +54,42 @@ class Generator(nn.Module):
             labels (batch_size, 1): Labels
         """
         one_hot = to_one_hot_batched(labels)
-        concatenated = torch.cat((noise, one_hot), 1)  # (batch_size, params.z_size + params.n_classes)
+        concatenated = torch.cat((noise, one_hot), 1).to(noise.device)  # (batch_size, params.z_size + params.n_classes)
         return self.main(concatenated.unsqueeze(-1).unsqueeze(-1))  # Add (1, 1) dimensions to the noise vector
 
 
 class Discriminator(nn.Module):
-    pass
+    def __init__(self, params: Parameters) -> None:
+        super().__init__()
+        self.params = params
+
+        self.main = nn.Sequential(
+            # (batch_size, params.n_classes, 28, 28) -> (batch_size, params.feature_map_size, 14, 14)
+            nn.Conv2d(params.n_classes, params.feature_map_size, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # (batch_size, params.feature_map_size, 14, 14) -> (batch_size, params.feature_map_size * 2, 7, 7)
+            nn.Conv2d(params.feature_map_size, params.feature_map_size * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(params.feature_map_size * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # (batch_size, params.feature_map_size * 2, 7, 7) -> (batch_size, 1, 1, 1)
+            nn.Conv2d(params.feature_map_size * 2, 1, 7, 1, 0, bias=False),
+            nn.Sigmoid(),
+            # output: (batch_size, 1, 1, 1)
+        )
+
+    def forward(self, images: Tensor, labels: Tensor) -> Tensor:
+        """Discriminate real from fake images
+        Args:
+            images (batch_size, params.channels, 28, 28): Images
+            labels (batch_size, 1): Labels
+
+        Returns:
+            (batch_size, 1): Predictions of the discriminator
+        """
+        # Create the (batch_size, params.n_classes, 28, 28) "one-hot" image tensor
+        encoded = torch.zeros((labels.size(0), self.params.n_classes, 28, 28), device=images.device)
+
+        for i, (image, label) in enumerate(zip(images, labels)):
+            encoded[i, int(label.item())] = image.squeeze()
+
+        return self.main(encoded).squeeze(-1).squeeze(-1)  # Remove the last 2 dimensions
